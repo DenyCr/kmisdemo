1c7878f00dc5a55e1cc87fcccbdd84a6c36a606f
DenyCr <denis01121994@gmail.com>
2019-11-21 21:42:13 +0300

    form

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .test
-  h1 привет  сосед &nbsp;
+  //- h1 привет  сосед &nbsp;
   //- .row
   //-   input(type="text") 
   //-   select(name="select" size="3" multiple="multiple")
@@ -8,6 +8,44 @@
   //-     option Крокодил Гена
   //-     option Шапокляк
   //-     option Крыса Лариса
+
+  div(class="persData" v-if="post")
+    
+    div(class="FIO")
+        p ФИО 
+        input
+    div(class="POL")
+        p Пол
+        select
+          option(disabled value="") Выберите один из вариантов
+          option Муж
+          option Жен
+        input(type="radio" id="one" value="Men" )
+        label(for="one") Муж
+        input(type="radio" id="two" value="Gl" )
+        label(for="two") Жен
+    div(class="HB")
+        p Дата рождения
+        input(type="date")
+    div(class="CNILS")
+        p СНИЛС
+        input
+    div(class="SerailPass" pattern="[1-5]{5}")
+        p Серия полиса
+        input
+    div(class="NomerPass")
+        p Номер полиса
+        input
+
+  div(class="contactInfo")     
+    div(class="email")
+        p электронная почта
+        input
+    div(class="phone" data-mask="+7(999)999-99-99")
+        p номер телефона для связи
+        input
+     
+
   div(class="post" v-if="post")
     div(class="post__title") 
       h1 {{ post.title }}
@@ -60,11 +98,18 @@ export default {
   background: #FFFFE4
   //display: flex
 
+.contactInfo
+  background: red
+
 
 h1 
   color: green
 
 .row
   display: flex
-  flex-direction: column  
+  flex-direction: column 
+
+.persData
+  // display: grid
+  //grid-template-columns: 20% auto
 </style>
